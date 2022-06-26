3f8c6b1b833fbedc6ce236365ba307ce6a0c7ddf
juanchicruzz <59696948+juanchicruzz@users.noreply.github.com>
2022-06-26 19:57:16 -0300

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,9 +1,9 @@
 <?php 
 
-	$cleardb_url = parse_url("mysql://be1c98da14aa83:377a3952@us-cdbr-east-05.cleardb.net/heroku_24549face6fa079?reconnect=true");
-	const DB_HOST = $cleardb_url["host"];
-    	const DB_USERNAME = $cleardb_url["user"];
-    	const DB_PASSWORD = $cleardb_url["pass"];
-    	const DB_NAME = substr($cleardb_url["path"],1);
+	#$cleardb_url = parse_url("mysql://be1c98da14aa83:377a3952@us-cdbr-east-05.cleardb.net/heroku_24549face6fa079?reconnect=true");
+	const DB_HOST = "us-cdbr-east-05.cleardb.net";
+    	const DB_USERNAME = "be1c98da14aa83";
+    	const DB_PASSWORD = "377a3952";
+    	const DB_NAME = "heroku_24549face6fa079";
     	const DB_CHARSET = 'utf8mb4';
 ?>
