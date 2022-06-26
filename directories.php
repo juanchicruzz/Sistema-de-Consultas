3423aba09eced9364a6a7a23bbbb7e03b6aed125
juanchicruzz <59696948+juanchicruzz@users.noreply.github.com>
2022-06-26 20:22:27 -0300

    Update directories.php

--- directories.php
+++ directories.php
@@ -22,18 +22,17 @@ define('DIR_SECURITY', $_SERVER['DOCUMENT_ROOT'] . ROOT . "/controllers/security
 define('DIR_VIEWS', $_SERVER['DOCUMENT_ROOT'] . ROOT . "/views");
 
 // REDIRECT (NO PARA INCLUIR ARCHIVOS)
+define('REDIR_INDEX', ROOT . "/index.php");
+define('REDIR_HEADER', ROOT . "/partials/header.php");
+define('REDIR_FOOTER', ROOT . "/partials/footer.php");
+define('REDIR_PARTIALS', ROOT . "/partials");
 
-define('REDIR_INDEX', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/index.php");
-define('REDIR_HEADER', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/partials/header.php");
-define('REDIR_FOOTER', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/partials/footer.php");
-define('REDIR_PARTIALS', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/partials");
-
-define('REDIR_AUTH', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/auth");
-define('REDIR_CONTROLLERS', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/controllers");
-define('REDIR_DB', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/db");
-define('REDIR_REPOSITORIES', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/db/repositories");
-define('REDIR_SECURITY', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/controllers/security.php");
-define('REDIR_VIEWS', $_SERVER['DOCUMENT_ROOT'] .ROOT . "/views");
+define('REDIR_AUTH', ROOT . "/auth");
+define('REDIR_CONTROLLERS', ROOT . "/controllers");
+define('REDIR_DB', ROOT . "/db");
+define('REDIR_REPOSITORIES', ROOT . "/db/repositories");
+define('REDIR_SECURITY', ROOT . "/controllers/security.php");
+define('REDIR_VIEWS', ROOT . "/views");
 
 
 ?>
