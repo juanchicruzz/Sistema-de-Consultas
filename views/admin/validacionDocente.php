95ffeee986486eaa6e09a69316c1ae1fe78e09af
juanchicruzz <59696948+juanchicruzz@users.noreply.github.com>
2022-07-05 19:38:07 -0300

    btn export pdf

--- views/admin/validacionDocente.php
+++ views/admin/validacionDocente.php
@@ -82,6 +82,9 @@ include(DIR_HEADER)
             ?>
            </tbody>
        </table>
+       <input class="btn btn-primary" type="button" id="btnExport" value="Descargar PDF" onclick="Export('tableValidarDocente','docentes_a_validar')" />
+       <br>
+       <br>
     </div>
 
 <?php include(DIR_FOOTER);?>
