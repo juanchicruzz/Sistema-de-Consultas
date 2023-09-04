19d34a78b90d81b62faf72b8bfed6b25cf05d478
juanchicruzz <59696948+juanchicruzz@users.noreply.github.com>
2023-09-04 18:15:43 -0300

    Add files via upload

---
+++ db/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+	
+	const DB_HOST = "us-cdbr-east-05.cleardb.net";
+    	const DB_USERNAME = "be1c98da14aa83";
+    	const DB_PASSWORD = getenv('DB_PASSWORD');
+    	const DB_NAME = "heroku_24549face6fa079";
+    	const DB_CHARSET = 'utf8mb4';
+?>
+
