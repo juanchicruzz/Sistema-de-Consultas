efd7d4853abc8e941cb343738b89cec49e720d69
juanchicruzz <59696948+juanchicruzz@users.noreply.github.com>
2023-09-18 10:19:04 -0300

    config

---
+++ db/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+	
+		const DB_HOST = "us-cdbr-east-05.cleardb.net";
+    	const DB_USERNAME = "be1c98da14aa83";
+    	const DB_NAME = "heroku_24549face6fa079";
+    	const DB_CHARSET = 'utf8mb4';
+?>
+
