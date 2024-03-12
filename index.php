a6810f8387a4369675c2d79719c0e8b1f7d35c37
jacciarri <accirrijoshua@gmail.com>
2024-03-12 17:07:36 -0300

    Cambios en la vista principal del profesor

--- index.php
+++ index.php
@@ -12,13 +12,22 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
 <div class="container">
     <div class="row">
-        <h1 class="my-5">Bienvenido al sitio! <b><?php echo htmlspecialchars($_SESSION["nombre"]." ". $_SESSION["apellido"] ); ?></b></h1>
+        <h1 class="my-5">Bienvenido al sitio, 
+            <b><?php echo htmlspecialchars($_SESSION["nombre"]." ". $_SESSION["apellido"] ); ?></b>!
+        </h1>
     </div>
     <div class="row">
         <div class="container">
             <div class="d-flex justify-content-between align-items-center">
+            <?php
+            if($_SESSION["userType"] == "2") {
+            ?>    <h4>Empezá a navegar por tus Horarios de Consultas</h4>
+                <a href="<?= REDIR_VIEWS ?>/profesor/ConsultasProfesor.php" class="btn btn-primary">Ver mis Consultas</a>
+                <?php
+            } else { ?>
                 <h4>Empezá a navegar por los Horarios de Consultas</h4>
                 <a href="<?= REDIR_VIEWS ?>/consultas.php" class="btn btn-primary">Ver Horarios</a>
+            <?php } ?>
             </div>
             <hr>
         </div>
