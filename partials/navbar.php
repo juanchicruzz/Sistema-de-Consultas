e6435c271a2bb4119d81675dc6304b2344839c73
juanchicruzz <59696948+juanchicruzz@users.noreply.github.com>
2024-03-13 18:50:55 -0300

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -2,7 +2,7 @@
 include_once($_SERVER['DOCUMENT_ROOT'] . "/directories.php");
 ?>
 
-<header class="p-3 mb-3 border-bottom bg-primary">
+<header class="p-3 mb-3 border-bottom">
   <div class="container-fluid no-padding">
     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
       <a href="<?= REDIR_INDEX ?>" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
