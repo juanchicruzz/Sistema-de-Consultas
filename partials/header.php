62cc6ed2b46aebb5a13b67998e37792e16e433e5
juanchicruzz <59696948+juanchicruzz@users.noreply.github.com>
2024-03-16 15:35:02 -0300

    google site ver

--- partials/header.php
+++ partials/header.php
@@ -6,6 +6,7 @@ include_once($_SERVER['DOCUMENT_ROOT'] . "/directories.php");
 
 <head>
   <meta charset="UTF-8">
+  <meta name="google-site-verification" content="-gIx6uCvgBv2Srzoh_-stK2hFZCQArfoJJIP59pa3Tk" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
